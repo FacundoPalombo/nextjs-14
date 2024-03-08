8a575c3dc4d2ca323f399b463509fdf95640f981
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-03-08 20:48:56 +0100

    feat: add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
 import Page from '@/app/page';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default function Customers() {
   return <p>Customers</p>;
